c54cddf26c9090857b902c730ec4abc139161549
Francesco Meneghini <francescofl.meneghini@gmail.com>
2018-05-08 16:09:50 +0200

    un altro messaggio

--- about_keyword_arguments.rb
+++ about_keyword_arguments.rb
@@ -1,7 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutKeywordArguments < Neo::Koan
-  # Un utilissimo commento
   def method_with_keyword_arguments(one: 1, two: 'two')
     [one, two]
   end
