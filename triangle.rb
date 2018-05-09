7f8056bfd5ba36b43b9c999255f12290bbedc53b
Francesco Meneghini <francescofl.meneghini@gmail.com>
2018-05-09 12:25:42 +0200

    Triangle

--- triangle.rb
+++ triangle.rb
@@ -13,8 +13,18 @@
 # and
 #   about_triangle_project_2.rb
 #
+def equilateral?(a, b, c)
+  a == b && b == c
+end
+
+def isosceles?(a, b, c)
+  a == b || b == c || a == c
+end
+
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  return :equilateral if equilateral?(a, b, c)
+  return :isosceles if isosceles?(a, b, c)
+  return :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
