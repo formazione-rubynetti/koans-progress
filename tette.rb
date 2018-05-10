1ef35d19114e227bb725ddbe35d91471501ddcc6
Francesco Meneghini <francescofl.meneghini@gmail.com>
2018-05-10 17:24:16 +0200

    Risolve problemi di inserimento della parola e aggiunge classe UmorismoRaffinato

--- tette.rb
+++ tette.rb
@@ -1,37 +1,43 @@
 class UmorismoSpicciolo
   # Commento scritto su github
-  @@parole_buffe = ["Simonini", "Tette", "Cacca"].map(&:downcase)
+  @@parole_buffe = ["simonini", "tette", "cacca"]
+
+  def initialize
+    @parole_buffe = @@parole_buffe
+  end
+
+  def parole_buffe
+    @parole_buffe
+  end
 
   def aggiungi_parola_buffa(parola)
-    @parole_buffe += parola
+    @parole_buffe.push(parola)
   end
 
   def parola_buffa?(parola, force: false, add: false)
-    aggiungi_parola_buffa(parola) if add
     parola = parola.downcase
-    if force || @@parole_buffe.include?(parola)
+    aggiungi_parola_buffa(parola) if add
+    if force || @parole_buffe.include?(parola)
       return "ahahaha, #{parola} che ridere"
     else
       return "buuu, #{parola} fai schifo"
     end
   end
 
-  private
-
-  def parole_buffe
-    @parole_buffe += []
-    @@parole_buffe + @parole_buffe
-  end
-
 end
 
-umorista = UmorismoSpicciolo.new
+class UmorismoRaffinato < UmorismoSpicciolo
+  @@parole_buffe = ["alessandro descovi", "seni", "feci"]
+end
+
+umorista = UmorismoRaffinato.new
 p umorista.parola_buffa?("Tette")
 p umorista.parola_buffa?("tette")
 p umorista.parola_buffa?("TEttE")
 p umorista.parola_buffa?("Tett")
 p umorista.parola_buffa?("Gino", add: true)
-p umorista.parola_buffa?("tristezza", force: true)
+p umorista.parola_buffa?("tristezza degli altri", force: true)
 p umorista.parola_buffa?("culo")
 p umorista.parola_buffa?("culo")
 p umorista.parola_buffa?("Gino")
+p umorista.parola_buffa?("Seni")
